a4a23477282a4a0c6bee3c56f69c8363e1d3e876
practiceDragon <goqkd2023@gmail.com>
2024-07-14 04:55:39 +0900

    치즈 후원이 켜져있지 않은 경우 종료 등 버그 수정

--- main.js
+++ main.js
@@ -11,8 +11,6 @@ const { delay } = require('./delay')
 const NAVER_ID = process.env.NAVER_ID
 const NAVER_PASSWORD = process.env.NAVER_PASSWORD
 const CHZZK_LIVE_LINK = process.env.CHZZK_LIVE_LINK
-const TEXT = process.env.TEXT
-
 
 async function startBrowser() {
   // 브라우저 객체 얻기
@@ -44,9 +42,36 @@ async function startBrowser() {
   // "치즈 아이콘"이 나타날 때까지 기다림 (최대 10초)
   await page.waitForSelector('#layout-body > section > aside > div.live_chatting_area__hUPJw > div.live_chatting_input_container__qA0ad > button:nth-child(3) > svg', { timeout: 10000 });
 
-  // 클릭
-  console.log('클릭')
-  await page.click('#layout-body > section > aside > div.live_chatting_area__hUPJw > div.live_chatting_input_container__qA0ad > button:nth-child(3) > svg');
+  // 아이콘의 셀렉터를 정의합니다.
+  const iconSelector = '#layout-body > section > aside > div.live_chatting_area__hUPJw > div.live_chatting_input_container__qA0ad > button:nth-child(3) > svg';
+  // 치즈 아이콘 클릭
+  console.log('치즈 아이콘 클릭')
+  const strokeWidth = await page.$eval(`${iconSelector} > path:first-child`, path => path.getAttribute('stroke-width'));
+
+  console.log('strokeWidth:', strokeWidth);
+  if(!strokeWidth) {
+    console.log('치즈 아이콘이 존재합니다')
+    await delay(10000) // 10초 대기
+    await page.click(iconSelector);
+
+
+    // 규칙사항의 셀렉터를 정의합니다.
+    const ruleSelector = '#layout-body > section > aside > div.live_chatting_popup_rule_container__gnQS8 > button.live_chatting_popup_rule_agree_button__cJJd3';
+    // 규칙사항이 존재하는지 확인합니다.
+    const rule = await page.$(ruleSelector) !== null;
+    if(rule) {
+      console.log('규칙이 존재합니다.')
+      await page.click(ruleSelector);
+      await delay(1000) // 1초 대기
+      console.log('치즈 아이콘을 다시 클릭합니다.')
+      await page.click(iconSelector);
+    }
+  } else {
+    console.log('해당 방송은 치즈 후원이 켜져있지 않습니다.');
+    console.log('치즈 후원을 켜주세요.')
+    await browser.close(); // 브라우저 닫기
+    return;
+  }
 
   // 3초 대기
   console.log('5초 대기')
@@ -56,15 +81,14 @@ async function startBrowser() {
   let lastText = '';
   while (true) {
     console.log('------- 시작 ---------')
-    await delay(500); // 1초마다 대기
+    await delay(1000); // 1초마다 대기
   
-    console.log('마우스를 올리기');
-    
+    console.log('치즈 후원창의 텍스트 입력필드에 커서를 올리기');
     // 처음에는 textarea에 포커스를 맞춥니다.
     let selector = first 
       ? '#layout-body > section > aside > div.live_chatting_area__hUPJw > div.live_chatting_popup_donation_container__-Xbda > div.live_chatting_popup_donation_content__W8X4L > div:nth-child(5) > div > div.live_chatting_donation_message_wrapper__SNoBH > div.live_chatting_donation_message_input_wrapper__04s9x > textarea'
       : '#layout-body > section > aside > div.live_chatting_area__hUPJw > div.live_chatting_popup_donation_container__-Xbda > div.live_chatting_popup_donation_content__W8X4L > div:nth-child(5) > div > div.live_chatting_donation_message_wrapper__SNoBH > div.live_chatting_donation_message_input_wrapper__04s9x > pre';
-  
+
     await page.focus(selector);
   
     console.log('텍스트 선택');
@@ -74,18 +98,18 @@ async function startBrowser() {
     });
   
     if (currentText && currentText !== lastText) {
+      console.log('텍스트 삭제');
+      await page.evaluate(() => {
+        const preElement = document.querySelector('pre');
+        preElement.innerText = ''; // pre 태그의 텍스트를 비움
+      });
+
       console.log('텍스트 입력');
       await page.keyboard.type(currentText, { delay: 100 }); // 텍스트 입력
       console.log('텍스트 입력 완료')
   
       console.log('스피커 아이콘 클릭');
       await page.click('#layout-body > section > aside > div.live_chatting_area__hUPJw > div.live_chatting_popup_donation_container__-Xbda > div.live_chatting_popup_donation_content__W8X4L > div:nth-child(4) > div > div.live_chatting_popup_donation_preview__TOHTj > button > svg');
-  
-      console.log('텍스트 삭제');
-      await page.evaluate(() => {
-        const preElement = document.querySelector('pre');
-        preElement.innerText = ''; // pre 태그의 텍스트를 비움
-      });
   
       lastText = currentText; // 이전 텍스트 업데이트
     } else {
@@ -96,7 +120,7 @@ async function startBrowser() {
     first = false; // 처음 실행 후에는 first 값을 false로 변경하여 다음에는 pre 요소에 포커스를 맞출 수 있게 합니다.
   }
 
-  // await browser.close(); // 브라우저 닫기
+  await browser.close(); // 브라우저 닫기
 }
 
 startBrowser();
